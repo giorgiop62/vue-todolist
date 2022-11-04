66baead2ed3993e4d941072e2efa4d9dda48db19
giorgip62 <giorgiop62@gmail.com>
2022-11-04 18:05:25 +0100

    createTask

--- java/script.js
+++ java/script.js
@@ -21,11 +21,37 @@ createApp({
         },
      
      
-       ]
+       ],
+       newString: '',
+       erroreStringa:'',
 
 
       
     }
+  },
+
+  methods:{
+    createTask(){
+      console.log(this.newString);
+      this.erroreStringa = '';
+      if(this.newString.length < 5){
+        this.erroreStringa = "Devi inserire almeno 5 caratteri"
+      }else{
+        const newTask = {
+          text: this.newString,
+          done: false
+        }
+        this.tasks.unshift(newString);
+        this.newString = '';
+      }
+
+    },
+
+    removeTask(index){
+      console.log(index);
+
+    }
+
   }
 
 
